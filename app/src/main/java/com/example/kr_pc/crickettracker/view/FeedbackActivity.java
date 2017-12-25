3ceb7a8621e1efcab7f36a8834ee5d91d9d8b180
Karthik R <karthikr1493@gmail.com>
2017-12-25 18:06:32 +0530

    Added firebase dependency to project. Feedback get stored in firebase real time database

--- app/src/main/java/com/example/kr_pc/crickettracker/view/FeedbackActivity.java
+++ app/src/main/java/com/example/kr_pc/crickettracker/view/FeedbackActivity.java
@@ -1,23 +1,35 @@
 package com.example.kr_pc.crickettracker.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.kr_pc.crickettracker.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class FeedbackActivity extends AppCompatActivity {
 
+    public static final String FEEDBACK = "feedback";
     Button submitButton;
     EditText feedbackText;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference(FEEDBACK);
 
         submitButton = findViewById(R.id.submit_button);
 
@@ -32,17 +44,31 @@ public class FeedbackActivity extends AppCompatActivity {
     private void onSubmitButtonClick() {
         feedbackText = findViewById(R.id.feedback_text);
 
-        if (feedbackText.getText().toString().trim().isEmpty()) {
-            Toast.makeText(getApplicationContext(), R.string.empty_feedback_text_message, Toast.LENGTH_LONG).show();
+        if (connectivityAvailable()) {
+
+            if (feedbackText.getText().toString().trim().isEmpty()) {
+                Toast.makeText(getApplicationContext(), R.string.empty_feedback_text_message, LENGTH_LONG).show();
+            } else {
+
+                myRef.push().setValue(feedbackText.getText().toString());
+                Toast.makeText(getApplicationContext(), R.string.feedback_message, LENGTH_LONG).show();
+                finish();
+            }
         } else {
-            Toast.makeText(getApplicationContext(), R.string.feedback_message, Toast.LENGTH_LONG).show();
-            finish();
+            Toast.makeText(getApplicationContext(), getString(R.string.no_internet_message), LENGTH_LONG).show();
         }
+
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.move_left_anim, R.anim.move_right_anim);
+    }
+
+    public boolean connectivityAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
