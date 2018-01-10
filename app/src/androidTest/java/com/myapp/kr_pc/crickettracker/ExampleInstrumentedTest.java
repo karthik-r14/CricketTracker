14d35641658b6b26b13f347e88a4d157b54d96cd
Karthik R <karthikr1493@gmail.com>
2018-01-10 11:12:16 +0530

    Restricting screen orientation to portrait.

--- app/src/androidTest/java/com/myapp/kr_pc/crickettracker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/myapp/kr_pc/crickettracker/ExampleInstrumentedTest.java
@@ -15,12 +15,12 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class myappInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.kr_pc.crickettracker", appContext.getPackageName());
+        assertEquals("com.myapp.kr_pc.crickettracker", appContext.getPackageName());
     }
 }
